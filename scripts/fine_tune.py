b93c91fe8b0891987c1a6fa3168ea3b0d092926a
Jake Koenig <jakethekoenig@gmail.com>
2024-02-13 09:51:33 -0800

    Add a script to finetune from jsonl

---
+++ scripts/fine_tune.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import argparse
+import os
+
+from openai import OpenAI
+
+client = OpenAI()
+import time
+
+parser = argparse.ArgumentParser(description="Fine tuning GPT-3")
+parser.add_argument(
+    "--file", type=str, default=None, help="Data file to use for fine tuning"
+)
+args = parser.parse_args()
+
+f = client.files.create(file=open(args.file, "rb"), purpose="fine-tune")
+
+print("Uploaded: ", f)
+while True:
+    time.sleep(30)
+    f = client.files.retrieve(f.id)
+    if f.status == "processed":
+        break
+
+print(client.fine_tuning.jobs.create(training_file=f.id, model="gpt-3.5-turbo-1106"))
