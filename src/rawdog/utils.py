fe57bda4851ad1440c411954f93d87f3a00b894a
granawkins <granthawkins88@gmail.com>
2024-02-17 11:05:39 +0700

    remove cwd_info from context

--- src/rawdog/utils.py
+++ src/rawdog/utils.py
@@ -26,7 +26,6 @@ class EnvInfo:
         self.cwd = data["cwd"]
         self.os = data["os"]
         self.is_git = data["is_git"]
-        self.cwd_info = data["cwd_info"]
         self.last_commit = data["last_commit"]
         self.retries = data["retries"]
 
@@ -36,7 +35,6 @@ class EnvInfo:
         self.os = platform.system()
         _is_git = Path(".git").exists()
         self.is_git = "IS" if _is_git else "is NOT"
-        self.cwd_info = self._get_cwd_info()
         self.last_commit = (
             ""
             if not _is_git
@@ -52,40 +50,15 @@ class EnvInfo:
         _retries = 0 if self.config is None else self.config.get("retries")
         self.retries = f"\nYou'll get {_retries} retries."
 
-    def _get_cwd_info(self, max_items=100):
-        output = []
-        for i, item in enumerate(self.cwd.iterdir()):
-            if i >= max_items:
-                break
-            name = ("" if not item.is_dir() else "/") + item.name
-            last_modified = datetime.datetime.fromtimestamp(
-                item.stat().st_mtime
-            ).strftime("%Y-%m-%d %H:%M:%S")
-            size, unit = 0, ""
-            try:
-                size = (
-                    len(list(item.iterdir())) if item.is_dir() else item.stat().st_size
-                )
-                unit = " bytes" if item.is_file() else " items"
-            except Exception:
-                pass
-            output.append(f"{last_modified} {size:10}{unit} {name}")
-        if not output:
-            return "The directory is empty."
-        return "\n".join(output)
-
     def render_prompt(self):
         return """\
 Today's date is {date}.
 The current working directory is {cwd}, which {is_git} a git repository.
-The user's operating system is {os}.
-The contents of the current working directory are:
-{cwd_info}{last_commit}{retries}""".format(
+The user's operating system is {os}.{last_commit}{retries}""".format(
             date=self.date,
             cwd=self.cwd,
             is_git=self.is_git,
             os=self.os,
-            cwd_info=self.cwd_info,
             last_commit=self.last_commit,
             retries=self.retries,
         )
