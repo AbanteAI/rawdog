39d9042779142fcf4abc7290b9e39269d2345884
granawkins <granthawkins88@gmail.com>
2024-02-17 14:48:02 +0700

    workflow improvements
    * always stream the script output
    * make terminal output (breaks, spaces, etc) more legible
    * remove duplicate prints
    * always say "Retrying..." on error

--- src/rawdog/execute_script.py
+++ src/rawdog/execute_script.py
@@ -36,39 +36,71 @@ def install_pip_packages(*packages: str):
     )
 
 
-def execute_script(script: str, llm_client) -> tuple[str, str, int]:
-    python_executable = get_rawdog_python_executable()
-    with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmp_script:
-        tmp_script_name = tmp_script.name
-        tmp_script.write(script)
-        tmp_script.flush()
+def _execute_script_in_subprocess(script) -> tuple[str, str, int]:
+    """Write script to tempfile, execute from .rawdog/venv, stream and return output"""
+    output, error, return_code = "", "", 0
+    try:
+        python_executable = get_rawdog_python_executable()
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmp_script:
+            tmp_script_name = tmp_script.name
+            tmp_script.write(script)
+            tmp_script.flush()
 
-        retry = True
-        while retry:
-            retry = False
-            result = subprocess.run(
-                [python_executable, tmp_script_name], capture_output=True, text=True
+            process = subprocess.Popen(
+                [python_executable, tmp_script_name],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                stdin=subprocess.DEVNULL,  # Raises EOF error if subprocess asks for input
+                text=True,
             )
-            output = result.stdout
-            error = result.stderr
-            return_code = result.returncode
-            if error and "ModuleNotFoundError: No module named" in error:
-                match = re.search(r"No module named '(\w+)'", error)
-                if match:
-                    module = match.group(1)
-                    module_name = llm_client.get_python_package(module)
-                    if (
-                        input(
-                            f"Rawdog wants to use {module_name}. Install to rawdog's"
-                            " venv with pip? (Y/n): "
-                        )
-                        .strip()
-                        .lower()
-                        != "n"
-                    ):
-                        install_result = install_pip_packages(module_name)
-                        if install_result.returncode == 0:
-                            retry = True
-                        else:
-                            print("Failed to install package")
+            while True:
+                _stdout = process.stdout.readline()
+                _stderr = process.stderr.readline()
+                if _stdout:
+                    output += _stdout
+                    print(_stdout, end="")
+                if _stderr:
+                    error += _stderr
+                    print(_stderr, end="", file=sys.stderr)
+                if _stdout == "" and _stderr == "" and process.poll() is not None:
+                    break
+            return_code = process.returncode
+    except Exception as e:
+        error += str(e)
+        print(e)
+        return_code = 1
     return output, error, return_code
+
+
+def _execute_script_with_dependency_resolution(
+    script, llm_client
+) -> tuple[str, str, int]:
+    retry = True
+    output, error, return_code = "", "", 0
+    while retry:
+        retry = False
+        output, error, return_code = _execute_script_in_subprocess(script)
+        if error and "ModuleNotFoundError: No module named" in error:
+            match = re.search(r"No module named '(\w+)'", error)
+            if match:
+                module = match.group(1)
+                module_name = llm_client.get_python_package(module)
+                if (
+                    input(
+                        f"Rawdog wants to use {module_name}. Install to rawdog's"
+                        " venv with pip? (Y/n): "
+                    )
+                    .strip()
+                    .lower()
+                    != "n"
+                ):
+                    install_result = install_pip_packages(module_name)
+                    if install_result.returncode == 0:
+                        retry = True
+                    else:
+                        print("Failed to install package")
+    return output, error, return_code
+
+
+def execute_script(script: str, llm_client) -> tuple[str, str, int]:
+    return _execute_script_with_dependency_resolution(script, llm_client)
